a2be8a1effe2fdca6b2eb5d2d9ffd16140a12395
Sebastian Vetter <sebastian@roadside-developer.com>
2012-11-05 13:19:17 +1100

    Merge Jon's twitter and video widgets

--- fancypages/models.py
+++ fancypages/models.py
@@ -549,3 +549,36 @@ class TabContainer(Container):
     def save(self, *args, **kwargs):
         self.variable_name = slugify(self.title)
         return super(TabContainer, self).save(*args, **kwargs)
+
+
+class VideoWidget(Widget):
+    name = _("Video")
+    code = 'video'
+    template_name = "fancypages/widgets/video.html"
+
+    SOURCE_YOUTUBE = 'youtube'
+    SOURCES = (
+        (SOURCE_YOUTUBE, _('YouTube video')),
+    )
+
+    source = models.CharField(_('Video Type'), choices=SOURCES, max_length=50)
+    video_code = models.CharField(_('Video Code'), max_length=50)
+
+    def __unicode__(self):
+        if self.source:
+            return "Video '%s'" % self.video_code
+        return "Video #%s" % self.id
+
+
+class TwitterWidget(Widget):
+    name = _("Twitter")
+    code = 'twitter'
+    template_name = "fancypages/widgets/twitter.html"
+
+    username = models.CharField(_('Twitter username'), max_length=50)
+    max_tweets = models.PositiveIntegerField(_('Maximum tweets'), default=5)
+
+    def __unicode__(self):
+        if self.username:
+            return u"Twitter user '@%s'" % self.username
+        return u"Twitter: %s" % self.id
