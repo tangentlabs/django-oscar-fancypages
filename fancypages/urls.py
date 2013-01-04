5c37c40fab88d9932b5e7d6a32e9a1d3d9582fe7
Sebastian Vetter <sebastian@roadside-developer.com>
2013-01-04 15:13:54 +1100

    Move page urls to end of patttern list

--- fancypages/urls.py
+++ fancypages/urls.py
@@ -5,7 +5,8 @@ from fancypages.app import application as fancypages_app
 from fancypages.dashboard.app import application as dashboard_app
 
 urlpatterns = patterns('',
-    url(r'^', include(fancypages_app.urls)),
     url(r'^dashboard/fancypages/', include(dashboard_app.urls)),
     url(fancypages.api.API_BASE_URL, include(fancypages.api.urls, 'fp-api')),
+
+    url(r'^', include(fancypages_app.urls)),
 )
