64c86305f36596cca8929b27eeb732caf6f8ab65
Sebastian Vetter <sebastian@roadside-developer.com>
2013-07-17 12:59:31 +1000

    Add dependency for migrations to fancypages.assets

--- oscar_fancypages/fancypages/migrations/0001_initial.py
+++ oscar_fancypages/fancypages/migrations/0001_initial.py
@@ -7,6 +7,10 @@ from django.db import models
 
 class Migration(SchemaMigration):
 
+    depends_on = (
+        ('assets', '0001_initial'),
+    )
+
     def forwards(self, orm):
         # Adding model 'AbstractPageType'
         db.create_table('fancypages_abstractpagetype', (
