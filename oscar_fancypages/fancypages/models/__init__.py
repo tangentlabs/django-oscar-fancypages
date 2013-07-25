1555dc56edeeccdb0541e01ccd74ea23319e45c5
Sebastian Vetter <sebastian@roadside-developer.com>
2013-07-25 11:35:48 +1000

    Overrride get_absolute_url for FancyPage
    Oscar 0.6+ will enforce a canoncial URL for a category and will
    redirect to the Category URL. Since fancypages uses a different
    URL and also treats the homepage differently (if set up), we need
    to override 'get_absolute_url' to redirect correctly.

--- oscar_fancypages/fancypages/models/__init__.py
+++ oscar_fancypages/fancypages/models/__init__.py
@@ -1,15 +1,25 @@
 from __future__ import absolute_import
 
-from django.db.models import get_model
+from django.db import models
+from django.conf import settings
+from django.template.defaultfilters import slugify
 
 from fancypages import manager
 from fancypages import abstract_models
 
-Category = get_model('catalogue', 'Category')
+Category = models.get_model('catalogue', 'Category')
 
 
 class FancyPage(Category, abstract_models.AbstractFancyPage):
     objects = manager.PageManager()
+
+    @models.permalink
+    def get_absolute_url(self):
+        # make sure that the home view is actually redirecting to '/'
+        # and not to '/home/'.
+        if self.slug == slugify(getattr(settings, 'FP_HOMEPAGE_NAME')):
+            return ('home', (), {})
+        return ('fancypages:page-detail', (), {'slug': self.slug})
 
 
 # We have to import all models from django-fancypages AFTER re-defining
