b90b37ef122ee0b21027bfe49ba761c6db4832fc
Sebastian Vetter <sebastian@roadside-developer.com>
2013-07-29 16:35:22 +1000

    Make use of FP mixins and move into separate module

--- oscar_fancypages/fancypages/views.py
+++ oscar_fancypages/fancypages/views.py
@@ -1,39 +1,18 @@
+from __future__ import absolute_import
+
 from django.http import Http404
-from django.conf import settings
 from django.db.models import get_model
-from django.template.defaultfilters import slugify
 
 from oscar.apps.catalogue.views import ProductCategoryView
+
+from . import mixins
 
 FancyPage = get_model('fancypages', 'FancyPage')
 Category = get_model('catalogue', 'Category')
 Container = get_model('fancypages', 'Container')
 
 
-class FancyPageEditorMixin(object):
-    DEFAULT_TEMPLATE = getattr(settings, 'FP_DEFAULT_TEMPLATE')
-
-    def get_template_names(self):
-        if not self.category.page_type:
-            return [self.DEFAULT_TEMPLATE]
-        return [self.category.page_type.template_name]
-
-    def get_object(self):
-        try:
-            return FancyPage.objects.get(slug=self.kwargs.get('slug'))
-        except (FancyPage.DoesNotExist, FancyPage.MultipleObjectsReturned):
-            raise Http404
-
-    def get_context_data(self, **kwargs):
-        ctx = super(FancyPageEditorMixin, self).get_context_data(**kwargs)
-        if self.category:
-            ctx['object'] = ctx[self.context_object_name] = self.category
-            for container in Container.get_containers(self.category):
-                ctx[container.name] = container
-        return ctx
-
-
-class FancyPageDetailView(FancyPageEditorMixin, ProductCategoryView):
+class FancyPageDetailView(mixins.OscarFancyPageMixin, ProductCategoryView):
     context_object_name = 'fancypage'
 
     def get_context_data(self, **kwargs):
@@ -68,32 +47,6 @@ class FancyPageDetailView(FancyPageEditorMixin, ProductCategoryView):
         return response
 
 
-class FancyHomeView(FancyPageEditorMixin, ProductCategoryView):
+class FancyHomeView(mixins.OscarFancyHomeMixin, ProductCategoryView):
     model = FancyPage
     context_object_name = 'fancypage'
-
-    HOMEPAGE_NAME = getattr(settings, 'FP_HOMEPAGE_NAME', 'Home')
-
-    def get(self, request, *args, **kwargs):
-        self.kwargs.setdefault('category_slug', slugify(self.HOMEPAGE_NAME))
-        self.category = self.get_object()
-        response = super(FancyHomeView, self).get(request, *args, **kwargs)
-        if request.user.is_staff:
-            return response
-
-        if not self.category.is_visible:
-            raise Http404
-
-        return response
-
-    def get_object(self):
-        slug = slugify(self.HOMEPAGE_NAME)
-        try:
-            page = FancyPage.objects.get(slug=slug)
-        except FancyPage.DoesNotExist:
-            page = FancyPage.add_root(
-                name=self.HOMEPAGE_NAME,
-                slug=slug,
-                status=FancyPage.PUBLISHED,
-            )
-        return page
