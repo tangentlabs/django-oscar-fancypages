e458152840e8f2fd917b0d9a2fe3dd8e3e69d4a9
Sebastian Vetter <sebastian@roadside-developer.com>
2013-08-14 16:57:05 +1000

    Add test for uploading image

--- tests/functional/page_tests.py
+++ tests/functional/page_tests.py
@@ -1,12 +1,22 @@
+import os
+import shutil
+import tempfile
+
+from PIL import Image
+
 from django.db.models import get_model
 from django.core.urlresolvers import reverse
+from django.test.utils import override_settings
 
-from django_webtest import WebTest
+from oscar_testsupport.testcases import WebTestCase
 
 FancyPage = get_model('fancypages', 'FancyPage')
 
+TEMP_IMAGE_DIR = tempfile.mkdtemp(suffix='_page_tests_images')
+TEMP_MEDIA_ROOT = tempfile.mkdtemp(suffix='_page_tests_media')
 
-class TestHomePage(WebTest):
+
+class TestHomePage(WebTestCase):
 
     def setUp(self):
         super(TestHomePage, self).setUp()
@@ -26,3 +36,41 @@ class TestHomePage(WebTest):
         container = fancypage.containers.all()[0]
         self.assertEquals(type(container.page_object), type(fancypage))
         self.assertEquals(container.page_object.id, fancypage.id)
+
+
+@override_settings(MEDIA_ROOT=TEMP_MEDIA_ROOT)
+class TestAFancyPage(WebTestCase):
+    is_staff = True
+
+    def tearDown(self):
+        super(TestAFancyPage, self).tearDown()
+        if os.path.exists(TEMP_MEDIA_ROOT):
+            shutil.rmtree(TEMP_MEDIA_ROOT)
+        if os.path.exists(TEMP_IMAGE_DIR):
+            shutil.rmtree(TEMP_IMAGE_DIR)
+
+    def test_can_be_updated_with_an_image(self):
+        fancy_page = FancyPage.add_root(name='Sample Page')
+        self.assertEquals(fancy_page.image, None)
+
+        im = Image.new("RGB", (320, 240), "red")
+
+        __, filename = tempfile.mkstemp(suffix='.jpg', dir=TEMP_IMAGE_DIR)
+        im.save(filename, "JPEG")
+
+        page = self.get(
+            reverse('fp-dashboard:page-update', args=(fancy_page.id,))
+        )
+
+        settings_form = page.form
+        settings_form['image'] = (filename,)
+        list_page = settings_form.submit()
+
+        self.assertRedirects(list_page, reverse('fp-dashboard:page-list'))
+
+        category_path = os.path.join(TEMP_MEDIA_ROOT, 'categories')
+        fancy_page = FancyPage.objects.get(id=fancy_page.id)
+        self.assertEquals(
+            fancy_page.image.path,
+            os.path.join(category_path, filename.rsplit('/')[-1])
+        )
